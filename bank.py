6bc68abf841b7a7afc1f72268ee2189c419718d3
Rafa <rafavazquez229@gmail.com>
2020-11-09 22:16:52 -0800

    set up my account to see if attrivutes work

--- bank.py
+++ bank.py
@@ -1,8 +1,27 @@
-class BankAccount: 
-    def __init__(self,full_name, account_balance, routing_number, balance):
-        self.full_name = full_name
-        self.account_balance = account_balance
-        self.routing_number = routing_number
-        self.balance = balance
+import random
 
-    def depostit(amount):
+class BankAccount: 
+    def __init__(self, full_name):
+        self.full_name = full_name
+        # only fullname goes in the parameters because the user inputs that 
+        self.account_number = random.randint(0, 999999)
+        self.routing_number = 987654321
+        self.balance = 0
+        
+
+# deposit method
+    def deposit(self, amount):
+        
+        self.balance += amount
+        print(f"Amount Deposited: ${amount}")
+
+
+    def withdraw(self,amount):
+        self.balance -= amount
+
+my_account = BankAccount("Rafa Vazquez")
+
+print(my_account.full_name)
+print(my_account.routing_number)
+print(my_account.account_number)
+print(my_account.balance)
