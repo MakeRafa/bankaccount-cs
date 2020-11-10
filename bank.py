071a12139f4ace816956da71580b1aae001241c3
Rafa <rafavazquez229@gmail.com>
2020-11-09 20:56:13 -0800

    class and attributes of bank
    tezt

---
+++ bank.py
@@ -0,0 +1,8 @@
+class BankAccount: 
+    def __init__(self,full_name, account_balance, routing_number, balance):
+        self.full_name = full_name
+        self.account_balance = account_balance
+        self.routing_number = routing_number
+        self.balance = balance
+
+    def depostit(amount):
